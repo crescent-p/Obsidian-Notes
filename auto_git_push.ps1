fbd84be5ea9c8a28d7e13947b63bb6b567268b60
Crescent P <109909716+crescent-p@users.noreply.github.com>
2024-10-25 22:02:20 +0530

    Auto-commit on 2024-10-25 22:02:20

--- auto_git_push.ps1
+++ auto_git_push.ps1
@@ -3,4 +3,4 @@ cd "C:\Users\cresc\Documents\Obsidian"  # Change to your repo path
 
 git add .                   # Stages all changes
 git commit -m "Auto-commit on $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"  # Creates a commit with a timestamp
-git push master # Replace 'main' with your branch name
+git push obsidian # Replace 'main' with your branch name
